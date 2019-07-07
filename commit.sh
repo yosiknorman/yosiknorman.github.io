915b728a9e80af74724909d067871a6751f23480
yosiknorman <yosikbrebes@gmail.com>
2019-07-07 21:31:16 +0800

    baru Min Jul  7 21:31:16 WITA 2019

---
+++ commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git status
+git add .
+git commit -m "baru `date`"
+git push -f --repo https://yosiknorman:brambang_07@github.com/yosiknorman/yosiknorman.github.io.git
+#cd ../cuda-workspace/mews_app/mews-sulsel
